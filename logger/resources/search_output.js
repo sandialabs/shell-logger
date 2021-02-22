05d24c0600918492f85fb8597bf81fddf317b6ca
David Collins <dcollin@sandia.gov>
2021-02-22 11:55:43 -0700

    Forgot to add search JS

---
+++ logger/resources/search_output.js
@@ -0,0 +1,44 @@
+function outputSearch(caller)
+{
+    // The parameter caller may be wither the search bar or the checkbox for
+    // "show duplicates"
+    var input, filter, table, tr, td, i, txtValue, lastTxtValue, checkbox, showDuplicates;
+    for (i = 0; i < caller.parentNode.childNodes.length; i++)
+    {
+        if (caller.parentNode.childNodes[i].type == "checkbox")
+        {
+            checkbox = caller.parentNode.childNodes[i];
+        }
+        if (caller.parentNode.childNodes[i].type == "text")
+        {
+            input = caller.parentNode.childNodes[i];
+        }
+    }
+    filter = input.value;
+    table = document.getElementById(input.getAttribute("target"));
+    tr = table.getElementsByTagName("tr");
+    showDuplicates = checkbox.checked;
+
+
+    // Loop through all table rows, and hide those who don't match the
+    // search query
+    for (i = 0; i < tr.length; i++)
+    {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td)
+        {
+            txtValue = td.textContent || td.innerText;
+            if (!filter ||
+                (txtValue.match(filter) && (!(txtValue === lastTxtValue) ||
+                                            showDuplicates)))
+            {
+                tr[i].style.display = "";
+                lastTxtValue = txtValue;
+            }
+            else
+            {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
