fd54ec79e49cfb0c5d8366e88bea0096566c9f7e
David Collins <dcollin@sandia.gov>
2021-03-05 17:36:25 -0700

    Changed output blocks so that they wrap in logger HTML

--- logger/resources/search_output.js
+++ logger/resources/search_output.js
@@ -2,7 +2,7 @@ function outputSearch(caller)
 {
     // The parameter caller may be wither the search bar or the checkbox for
     // "show duplicates"
-    var input, filter, table, tr, td, i, txtValue, lastTxtValue, checkbox, showDuplicates;
+    var input, filter, code, pre, i, txtValue, lastTxtValue, checkbox, showDuplicates;
     for (i = 0; i < caller.parentNode.childNodes.length; i++)
     {
         if (caller.parentNode.childNodes[i].type == "checkbox")
@@ -15,32 +15,31 @@ function outputSearch(caller)
         }
     }
     filter = input.value.toUpperCase();
-    table = document.getElementById(input.getAttribute("target"));
-    tr = table.getElementsByTagName("tr");
+    code = document.getElementById(input.getAttribute("target"));
+    pre = code.getElementsByTagName("pre");
+    div = code.querySelectorAll('[line-number]');
     showDuplicates = checkbox.checked;
 
 
-    // Loop through all table rows, and hide those who don't match the
+    // Loop through all code rows, and hide those who don't match the
     // search query
-    for (i = 0; i < tr.length; i++)
+    for (i = 0; i < pre.length; i++)
     {
-        td = tr[i].getElementsByTagName("td")[0];
-        if (td)
+        txtValue = pre[i].textContent || pre[i].innerText;
+        txtValue = txtValue.replaceAll("<[^>]*>", "");
+        txtValue = txtValue.replaceAll("\u2060", "").toUpperCase();
+        if (!filter ||
+            (txtValue.match(filter) && (!(txtValue === lastTxtValue) ||
+                                        showDuplicates)))
         {
-            txtValue = td.textContent || td.innerText;
-            txtValue = txtValue.replaceAll("<[^>]*>", "");
-            txtValue = txtValue.replaceAll("\u2060", "").toUpperCase();
-            if (!filter ||
-                (txtValue.match(filter) && (!(txtValue === lastTxtValue) ||
-                                            showDuplicates)))
-            {
-                tr[i].style.display = "";
-                lastTxtValue = txtValue;
-            }
-            else
-            {
-                tr[i].style.display = "none";
-            }
+            pre[i].style.display = "";
+            div[i].style.display = "";
+            lastTxtValue = txtValue;
+        }
+        else
+        {
+            pre[i].style.display = "none";
+            div[i].style.display = "none";
         }
     }
 }
