eec0844c4c6d747154e511e206926c3c6de56233
David Collins <dcollin@sandia.gov>
2021-03-08 20:10:08 -0700

    Improved search
    * Added a search button (interactive search-as-you-type is too slow
      for large inputs)
    * Added a "show nearby" option to show lines near a matching line.

--- logger/resources/search_output.js
+++ logger/resources/search_output.js
@@ -2,12 +2,20 @@ function outputSearch(caller)
 {
     // The parameter caller may be wither the search bar or the checkbox for
     // "show duplicates"
-    var input, filter, table, tr, td, i, txtValue, lastTxtValue, checkbox, showDuplicates;
+    var input, filter, table, tr, td, i, txtValue, lastTxtValue, showDuplicates, showNeighbors;
     for (i = 0; i < caller.parentNode.childNodes.length; i++)
     {
         if (caller.parentNode.childNodes[i].type == "checkbox")
         {
-            checkbox = caller.parentNode.childNodes[i];
+            var checkbox = caller.parentNode.childNodes[i]
+            if (checkbox.name.toLowerCase().indexOf("duplicate") > -1)
+            {
+                showDuplicates = caller.parentNode.childNodes[i].checked;
+            }
+            if (checkbox.name.toLowerCase().indexOf("neighbor") > -1)
+            {
+                showNeighbors = caller.parentNode.childNodes[i].checked;
+            }
         }
         if (caller.parentNode.childNodes[i].type == "text")
         {
@@ -17,8 +25,16 @@ function outputSearch(caller)
     filter = input.value.toUpperCase();
     table = document.getElementById(input.getAttribute("target"));
     tr = table.getElementsByTagName("tr");
-    showDuplicates = checkbox.checked;
-
+    lastTxtValue = null
+    neighborOpacity = [
+        { offset: -3, opacity: 0.30 },
+        { offset: -2, opacity: 0.40 },
+        { offset: -1, opacity: 0.50 },
+        { offset:  1, opacity: 0.50 },
+        { offset:  2, opacity: 0.40 },
+        { offset:  3, opacity: 0.30 },
+    ]
+    matchingIndexes = []
 
     // Loop through all table rows, and hide those who don't match the
     // search query
@@ -33,11 +49,27 @@ function outputSearch(caller)
                                         showDuplicates)))
         {
             tr[i].style.display = "";
+            tr[i].style.opacity = 1.0;
             lastTxtValue = txtValue;
+            matchingIndexes.push(i)
         }
         else
         {
             tr[i].style.display = "none";
+            tr[i].style.opacity = 0.0;
+        }
+    }
+    lastTxtValue = null
+    if (showNeighbors)
+    {
+        for (const i of matchingIndexes)
+        {
+            for (const op of neighborOpacity)
+            {
+                j = Math.min(Math.max(i+op.offset, 0), tr.length-1)
+                tr[j].style.display = "";
+                tr[j].style.opacity = Math.max(tr[j].style.opacity, op.opacity);
+            }
         }
     }
 }
