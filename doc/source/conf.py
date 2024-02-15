a96e8ae76cc2476762be77897e16eeb80e786afe
Jason M. Gates <jmgate@sandia.gov>
2024-02-15 07:58:34 -0700

    docs: Prepare for ReadTheDocs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,17 +1,9 @@
-#!/usr/bin/env python3
+"""
+Configuration file for the Sphinx documentation builder.
 
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
 from pathlib import Path
 import sys
 
@@ -31,9 +23,6 @@ release = version
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named `sphinx.ext.*`) or your custom
-# ones.
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.coverage",
@@ -43,24 +32,21 @@ extensions = [
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
     "sphinxarg.ext",
+    "sphinx_autodoc_typehints",
+    "sphinx_copybutton",
     "sphinx_rtd_theme",
+    "sphinxcontrib.programoutput",
+    "sphinxcontrib.spelling",
 ]
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
 # -- AutoDoc Configuration ---------------------------------------------------
 
